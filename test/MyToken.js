4b1c751818642bdc2b8cfca5e26ce8a2a0f2391d
Juan Xavier Valverde <juanxaviervm@hotmail.com>
2023-11-15 12:14:23 -0500

    updated test (#3)

--- test/MyToken.js
+++ test/MyToken.js
@@ -1,4 +1,3 @@
-const { anyValue } = require("@nomicfoundation/hardhat-chai-matchers/withArgs")
 const { expect } = require("chai")
 
 describe("MyToken", function () {
@@ -11,9 +10,11 @@ describe("MyToken", function () {
         myToken = await (await ethers.getContractFactory("MyToken")).deploy(owner.address)
     })
 
-    async function batchMint(to, amount) {
+    async function batchMintLogGas(to, amount, log) {
         for (let i = 1; i <= amount; i++) {
-            await myToken.safeMint(to, tokenID)
+            const receipt = await myToken.safeMint(to, tokenID)
+            const tx = await receipt.wait()
+            if (log) console.log(`\tGas used to mint token ${tokenID} was:`, String(tx.gasUsed))
             tokenID++
         }
     }
@@ -34,29 +35,23 @@ describe("MyToken", function () {
 
     describe("Minting, burning and populating data structures", function () {
         it("Should populate data structures as expected", async function () {
-            /* ------------------------ MINT ------------------------ */
-            await batchMint(alice.address, 3)
-            await batchMint(bob.address, 3)
-            await batchMint(carla.address, 3)
-
-            /* ---------------- GET INFO ---------------- */
+            await batchMintLogGas(alice.address, 3, true)
+            await batchMintLogGas(bob.address, 3, true)
+            await batchMintLogGas(carla.address, 3, true)
             log("")
             log("Alice's tokens are", await getAllTokensFromAddress(alice.address))
             log("Bob's tokens are", await getAllTokensFromAddress(bob.address))
             log("Carla's tokens are", await getAllTokensFromAddress(carla.address))
             log("")
             log("TOTAL SUPPLY OF TOKENS IS: ", (await myToken.totalSupply()).toString(), await getAllTokens())
-
             log("")
             log("**********************************************")
             log("Alice transfers her token ID 1 to Carla")
             log("**********************************************")
             log("")
-
             await myToken.connect(alice).transferFrom(alice.address, carla.address, 1)
             log("Alice's tokens after transferring are", await getAllTokensFromAddress(alice.address))
             log("Carla's tokens after transferring are", await getAllTokensFromAddress(carla.address))
-
             log("")
             log("ARRANGEMENT OF TOTAL SUPPLY OF TOKENS AFTER TRANSFERRING DOES NOT CHANGE: ", await getAllTokens())
             log("")
@@ -65,14 +60,13 @@ describe("MyToken", function () {
             log("**********************************************")
             log("")
             await myToken.connect(bob).burn(4)
-
             log("Bob's tokens after burning", await getAllTokensFromAddress(bob.address))
             log("ARRANGEMENT OF TOTAL SUPPLY OF TOKENS AFTER BURNING DOES CHANGE: ", await getAllTokens())
-        })
 
-        it("Should populate data structures as expected", async function () {
-            /* ------------------------ MINT ------------------------ */
-            await batchMint(alice.address, 1e6)
+            expect(await myToken.balanceOf(alice.address)).to.equal(2)
+            expect(await myToken.balanceOf(bob.address)).to.equal(2)
+            expect(await myToken.balanceOf(carla.address)).to.equal(4)
+            expect(await myToken.totalSupply()).to.equal(8)
         })
     })
 })
